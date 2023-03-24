c698370da1a947dd283829f60c886e0257339fb6
Patrick Brady <pbrady@atlassian.com>
2023-03-24 14:11:39 -0700

    adding to the useFeatures getter too

--- ui/src/hooks/useFeatures.ts
+++ ui/src/hooks/useFeatures.ts
@@ -4,7 +4,10 @@ import { listFeatures } from '../services/invokes';
 import { DefaultErrorTypes, ErrorTypes } from '../resolverTypes';
 
 export const useFeatures = (): [FeaturesList, boolean, ErrorTypes | undefined] => {
-  const [features, setFeatures] = useState<FeaturesList>({ [GitlabFeaturesEnum.SEND_STAGING_EVENTS]: false });
+  const [features, setFeatures] = useState<FeaturesList>({
+    [GitlabFeaturesEnum.SEND_STAGING_EVENTS]: false,
+    [GitlabFeaturesEnum.CREATE_FROM_YAML]: false,
+  });
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<ErrorTypes>();
 
