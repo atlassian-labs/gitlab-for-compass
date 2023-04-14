8b1df54a49cc5acd286207ac0d3e201b1b26b89e
Hitesh Sethiya <hsethiya@atlassian.com>
2023-04-14 16:16:27 -0700

    A fix for `replaceAll` not being supported by older browsers. Updated test case for verification.

--- ui/src/components/utils.test.ts
+++ ui/src/components/utils.test.ts
@@ -1,15 +1,19 @@
-import { capitalize } from './utils';
+import { capitalizeAndReplaceUnderscoresWithSpaces } from './utils';
 
 describe('utils', () => {
   it('Capitalize: capitalize lowercase text', async () => {
-    expect(capitalize('test')).toBe('Test');
+    expect(capitalizeAndReplaceUnderscoresWithSpaces('test')).toBe('Test');
   });
 
   it('Capitalize: capitalize uppercase text', async () => {
-    expect(capitalize('TEST')).toBe('Test');
+    expect(capitalizeAndReplaceUnderscoresWithSpaces('TEST')).toBe('Test');
   });
 
   it('Capitalize: replace _ with a <-SPACE->', async () => {
-    expect(capitalize('MACHINE_LEARNING_MODEL')).toBe('Machine learning model');
+    expect(capitalizeAndReplaceUnderscoresWithSpaces('MACHINE_LEARNING_MODEL')).toBe('Machine learning model');
+  });
+
+  it('Capitalize: empty string', async () => {
+    expect(capitalizeAndReplaceUnderscoresWithSpaces('')).toBe('');
   });
 });
