15507f55abccb9d0ca3099a16c512ab39b5a3f50
Erica Zhong <ezhong2@atlassian.com>
2023-05-15 16:56:43 -0400

    add message

--- src/entry/data-provider/index.ts
+++ src/entry/data-provider/index.ts
@@ -58,7 +58,7 @@ export const dataProvider = async (
     backfillData.metrics.mrCycleTime = mrCycleTime;
     backfillData.metrics.openMergeRequestsCount = openMergeRequestsCount;
   } catch (err) {
-    console.log(`data provider error: ${err.message} status: ${err.status}`);
+    console.log(`data provider error: ${err.message}`);
     const invocationErrorOptions = { backoffTimeInSeconds: 3600 };
 
     if (err instanceof GitlabHttpMethodError) {
