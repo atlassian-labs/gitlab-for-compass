859bc5352b4b86d54a6ec495abd6f5cae40b45ab
Shresta Bangaru <sbangaru@xhk03ch6qj.office.atlassian.com>
2023-08-09 16:28:29 -0400

    Adding a function to truncate project name in descriptions that are too long

--- src/services/deployment.ts
+++ src/services/deployment.ts
@@ -49,6 +49,18 @@ const isCompletedDeployment = (state: CompassDeploymentEventState) => {
   return state === CompassDeploymentEventState.Failed || state === CompassDeploymentEventState.Successful;
 };
 
+export const DESCRIPTION_TRUNCATION_LENGTH = 255;
+
+export const truncateProjectNameString = (beforeString: string, projectName: string, afterString: string) => {
+  // Spaces need to be included in `beforeString` and `afterString` so they can be considered in string length
+  let truncatedProjectName = projectName;
+  if (beforeString.length + projectName.length + afterString.length > DESCRIPTION_TRUNCATION_LENGTH) {
+    const projectNameLen = DESCRIPTION_TRUNCATION_LENGTH - beforeString.length - afterString.length;
+    truncatedProjectName = projectName.slice(0, projectNameLen);
+  }
+  return `${beforeString}${truncatedProjectName}${afterString}`;
+};
+
 export const gitlabApiDeploymentToCompassDeploymentEvent = (
   deployment: Deployment,
   projectId: number,
