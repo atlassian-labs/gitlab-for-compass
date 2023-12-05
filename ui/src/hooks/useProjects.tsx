7e0634cd5e8a839f842a122f4711d7407012229c
vbihun <vbihun@atlassian.com>
2023-12-05 17:29:43 +0200

    Fix issue with component type selector

--- ui/src/hooks/useProjects.tsx
+++ ui/src/hooks/useProjects.tsx
@@ -1,37 +1,47 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { ProjectImportSelection } from '../services/types';
 import { getComponentTypeOption } from '../components/utils';
 
-export const useProjects = (projects: ProjectImportSelection[]): ProjectImportSelection[] => {
-  const [selectedProjects, setSelectedProjects] = useState<ProjectImportSelection[]>([]);
+type UseProjectsReturnType = {
+  changedProjects: ProjectImportSelection[];
+  setChangedProjects: Dispatch<SetStateAction<ProjectImportSelection[]>>;
+};
+
+export const useProjects = (projects: ProjectImportSelection[]): UseProjectsReturnType => {
+  const [changedProjects, setChangedProjects] = useState<ProjectImportSelection[]>([]);
 
   useEffect(() => {
     if (projects.length !== 0) {
       projects.forEach((project) => {
-        const itemInSelectedList = selectedProjects.find((selectedProject) => selectedProject.id === project.id);
-        const isSelectedItemExists = Boolean(itemInSelectedList);
-        const shouldDeleteProjectFromList = isSelectedItemExists && !project.isSelected;
-        const shouldAddProjectToList = !isSelectedItemExists && project.isSelected;
-        const shouldUpdateProjectInList = itemInSelectedList && project.isSelected;
+        const itemInChangedList = changedProjects.find((changedProject) => changedProject.id === project.id);
 
-        if (shouldDeleteProjectFromList) {
-          setSelectedProjects((prevState) => prevState.filter((selectedItem) => project.id !== selectedItem.id));
-          return;
-        }
+        const isSelectedItemExists = Boolean(itemInChangedList);
+        const isComponentTypeChanged = itemInChangedList
+          ? JSON.stringify(itemInChangedList.typeOption) !== JSON.stringify(project.typeOption)
+          : false;
+        const isSelectChanged = itemInChangedList ? itemInChangedList.isSelected !== project.isSelected : false;
+        const isProjectChanged = project.isSelected || isComponentTypeChanged;
+        const isProjectUpdated = isSelectChanged || isComponentTypeChanged;
+
+        const shouldAddProjectToList = !isSelectedItemExists && isProjectChanged;
+        const shouldUpdateProjectInList = itemInChangedList && isProjectUpdated;
 
         if (shouldUpdateProjectInList) {
-          setSelectedProjects((prevState) =>
-            prevState.map((el) => (el.id === itemInSelectedList.id ? { ...el, typeOption: project.typeOption } : el)),
+          setChangedProjects((prevState) =>
+            prevState.map((el) =>
+              el.id === itemInChangedList.id
+                ? { ...el, typeOption: project.typeOption, isSelected: project.isSelected }
+                : el,
+            ),
           );
           return;
         }
 
         if (shouldAddProjectToList) {
-          setSelectedProjects((prevState) => [
+          setChangedProjects((prevState) => [
             ...prevState,
             {
               ...project,
-              id: project.id,
               typeOption: project.typeOption || getComponentTypeOption(project.typeId),
             },
           ]);
@@ -40,5 +50,5 @@ export const useProjects = (projects: ProjectImportSelection[]): ProjectImportSe
     }
   }, [projects]);
 
-  return selectedProjects;
+  return { changedProjects, setChangedProjects };
 };
