d5798336622d22405bb33629bd9b2d60707a2757
Anastasiia Penia <apenia@atlassian.com>
2024-01-08 11:12:28 +0100

    add owner team feature flag

--- src/features.ts
+++ src/features.ts
@@ -1,5 +1,6 @@
 export enum GitlabFeaturesEnum {
   SEND_STAGING_EVENTS = 'isSendStagingEventsEnabled',
+  OWNER_TEAM = 'isOwnerTeamEnabled',
 }
 
 export type FeaturesList = { [key in GitlabFeaturesEnum]: boolean };
