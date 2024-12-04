79942247ae6f947ced42bb5b42de04f74c3b32ec
Subbu Venkateswaran <svenkateswaran@atlassian.com>
2024-12-03 16:31:16 -0800

    Setup Gitlab Maintainer Token FF

--- src/features.ts
+++ src/features.ts
@@ -2,6 +2,7 @@ export enum GitlabFeaturesEnum {
   SEND_STAGING_EVENTS = 'isSendStagingEventsEnabled',
   DATA_COMPONENT_TYPES = 'isDataComponentTypesEnabled',
   DISABLE_DOCUMENT_COMPONENT_LINKS = 'isDocumentComponentLinksDisabled',
+  ENABLE_GITLAB_MAINTAINER_TOKEN = 'isGitlabMaintainerTokenEnabled',
 }
 
 export type FeaturesList = { [key in GitlabFeaturesEnum]: boolean };
