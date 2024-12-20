e68b84a4d4c0cbebe1d9df75587644b37040b203
Patrick Brady <pbrady@atlassian.com>
2024-12-20 15:06:53 -0800

    add type duplication to build command

--- src/features.ts
+++ src/features.ts
@@ -1,3 +1,9 @@
+/*
+  TO EDIT THIS FILE, you must edit the file in the repo's src directory and running yarn ui:build or yarn ui:prebuild will generate the types in the ui/src directory.
+
+  This file contains types that are used in both of the directories and need to stay in sync. So, the ui/src types are gitignored
+ */
+
 export enum GitlabFeaturesEnum {
   SEND_STAGING_EVENTS = 'isSendStagingEventsEnabled',
   DATA_COMPONENT_TYPES = 'isDataComponentTypesEnabled',
