e21c3b08e762b4a10aef17fb9f727ec8248e5d4b
Saahiti Vankayalapati <svankayalapati@atlassian.com>
2025-01-27 10:23:47 -0800

    removed internal ticket ref

--- src/entry/import-recent-repos/index.ts
+++ src/entry/import-recent-repos/index.ts
@@ -2,7 +2,6 @@ import { ImportRecentReposPayload, ImportRecentReposReturn } from './types';
 
 /**
  * Dummy handler function to import recent repositories.
- * TODO in https://softwareteams.atlassian.net/browse/COMPASS-23772
  *
  * @param payload - The payload containing the number of recent repositories to import.
  * @returns An object indicating the success of the operation and the number of repositories imported.
