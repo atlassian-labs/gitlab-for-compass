b606f3c57ff185d85705831af891b527f4e84436
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-04-06 01:44:57 +0000

    ADTEAM-486:  feat: Add GitLab for Compass logo variations (color, mono, inverted)

--- ui/src/components/assets/index.ts
+++ ui/src/components/assets/index.ts
@@ -1,3 +1,8 @@
 export { COMPONENT_TYPES } from './component-glyphs';
 
 export type { CompassIconsProps } from './types';
+
+// GitLab for Compass logos
+export { default as GitLabForCompassLogo } from './GitLabForCompass.svg';
+export { default as GitLabForCompassLogoMono } from './GitLabForCompass-mono.svg';
+export { default as GitLabForCompassLogoInverted } from './GitLabForCompass-inverted.svg';
