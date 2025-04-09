7bf52a33eb5131553ad353d4d199e55a1a501d15
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-04-09 01:54:44 +0000

    ADTEAM-521:  feat: Add GitLab Compass logo variants (icon, full color, monochrome)

--- ui/src/components/assets/index.ts
+++ ui/src/components/assets/index.ts
@@ -1,3 +1,8 @@
 export { COMPONENT_TYPES } from './component-glyphs';
 
 export type { CompassIconsProps } from './types';
+
+// GitLab Compass Logos
+export const GITLAB_COMPASS_LOGO = '/assets/gitlab-compass-logo.svg';
+export const GITLAB_COMPASS_MONO = '/assets/gitlab-compass-mono.svg';
+export const GITLAB_COMPASS_ICON = '/assets/gitlab-compass-icon.svg';
